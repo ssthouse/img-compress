4e4343e3cd6e9efd4ce3a86d07f90cc1ffa0a498
ssthouse <ssthouse@163.com>
2019-05-31 17:37:02 +0800

    Refactor index file code

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,19 @@ const imageCompresser = require("../lib/image-compresser");
 const KeyManager = require("../lib/key-manager");
 const keyManager = new KeyManager();
 
+if (process.argv.length <= 2) {
+  console.log(` 
+  ### 使用方法 ###
+
+  设置API key: img-process init -key ******
+  注: 请前往 https://tinypng.com/developers 获取 API key
+
+  压缩图片:     img-process imgFilePath
+  注: 同一key 每月有500次使用限制, 压缩图片将生成在原图片同一目录中
+  `);
+  return;
+}
+
 function isInitCmd() {
   for (let arg of process.argv) {
     if (arg.indexOf("init") !== -1) {
@@ -29,8 +42,6 @@ function getApiKeyFromArgv() {
   return process.argv[keyArgIndex];
 }
 
-console.log(`running~~~~`);
-
 if (isInitCmd()) {
   const apiKeyFromArg = getApiKeyFromArgv();
   if (!apiKeyFromArg) return;
@@ -38,9 +49,13 @@ if (isInitCmd()) {
 } else {
   if (!keyManager.getKey()) {
     console.log(
-      `before use it, please config the api-ke, for example: "img-compress init -key *******"`
+      `使用压缩图片功能前, 请先配置api key: "img-compress init -key *******"`
     );
   }
+  runCompress();
+}
+
+function runCompress() {
   console.log(`压缩ing......`);
   const apiKey = keyManager.getKey();
   const tinify = require("tinify");
@@ -48,7 +63,7 @@ if (isInitCmd()) {
   const originFilePath = process.argv[2];
   let destFilePath = originFilePath.replace(".png", "_compressed.png");
   destFilePath = destFilePath.replace(".jpg", "_compressed.jpg");
-  console.log(`原图: ${originFilePath}    结果图: ${destFilePath}`);
+  // console.log(`原图: ${originFilePath}    结果图: ${destFilePath}`);
   tinify
     .fromFile(originFilePath)
     .toFile(destFilePath)
