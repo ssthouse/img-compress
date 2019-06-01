8abc49b8dd622c39e4d31d8dde18c143e368b5dc
ssthouse <ssthouse@163.com>
2019-06-01 13:36:47 +0800

    Refactor index.js file

--- bin/index.js
+++ bin/index.js
@@ -1,22 +1,31 @@
 #!/usr/bin/env node
-
-const colors = require("colors");
 const imageCompresser = require("../lib/image-compresser");
 const KeyManager = require("../lib/key-manager");
 const keyManager = new KeyManager();
+const consoleUtil = require('../lib/console-util');
 
-if (process.argv.length <= 2) {
-  console.log(` 
-  ### 使用方法 ###
 
-  设置API key: img-process init -key ******
-  注: 请前往 https://tinypng.com/developers 获取 API key
+function isEmptyArg() {
+  return process.argv.length <= 2;
+}
 
-  压缩图片:     img-process imgFilePath
-  注: 同一key 每月有500次使用限制, 压缩图片将生成在原图片同一目录中
-  `);
+if (isEmptyArg()) {
+  consoleUtil.printDoc();
   return;
 }
+
+if (isInitCmd()) {
+  const apiKeyFromArg = getApiKeyFromArgv();
+  if (!apiKeyFromArg) return;
+  keyManager.setKey(apiKeyFromArg);
+} else {
+  if (!keyManager.getKey()) {
+    consoleUtil.printInitKeyHint();
+  }
+  const imgPath = process.argv[2];
+  imageCompresser(imgPath);
+}
+
 
 function isInitCmd() {
   for (let arg of process.argv) {
@@ -34,46 +43,8 @@ function getApiKeyFromArgv() {
       return arg === "-key";
     }) + 1;
   if (keyArgIndex >= process.argv.length) {
-    console.log(
-      `please enter api-key, for example: "img-compress init -key *******"`
-    );
+    consoleUtil.printInitKeyHint();
     return "";
   }
   return process.argv[keyArgIndex];
-}
-
-if (isInitCmd()) {
-  const apiKeyFromArg = getApiKeyFromArgv();
-  if (!apiKeyFromArg) return;
-  keyManager.setKey(apiKeyFromArg);
-} else {
-  if (!keyManager.getKey()) {
-    console.log(
-      `使用压缩图片功能前, 请先配置api key: "img-compress init -key *******"`
-    );
-  }
-  runCompress();
-}
-
-function runCompress() {
-  console.log(`压缩ing......`);
-  const apiKey = keyManager.getKey();
-  const tinify = require("tinify");
-  tinify.key = apiKey;
-  tinify.proxy = "http://dev-proxy.oa.com:8080";
-  const originFilePath = process.argv[2];
-  let destFilePath = originFilePath.replace(".png", "_compressed.png");
-  destFilePath = destFilePath.replace(".jpg", "_compressed.jpg");
-  destFilePath = destFilePath.replace(".jpeg", "_compressed.jpeg");
-  // console.log(`原图: ${originFilePath}    结果图: ${destFilePath}`);
-  tinify
-    .fromFile(originFilePath)
-    .toFile(destFilePath)
-    .then(() => {
-      console.log(`图片压缩成功`);
-    })
-    .catch(e => {
-      console.log(`图片压缩失败: ${e.message}`);
-      console.log(e);
-    });
 }
